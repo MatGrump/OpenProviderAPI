1b66f0f2620cb8e04aeaa58a51a92a9591fa1901
Mat.Grump <Mat@grumpy.tech>
2021-01-19 15:41:20 +0000

    Version 1.0 initial script

--- Get-BearerCreds.ps1
+++ Get-BearerCreds.ps1
@@ -1,12 +1,21 @@
-function Get-BearereCreds
+function Get-BearerCreds
 {
+
+    Param(
+        [parameter(Mandatory=$true)]
+        [String]$UserName,
+        [parameter(Mandatory=$true)]
+        [String]$Password 
+        )
+    
+
 $EndPoint = "https://api.openprovider.eu/v1beta/auth/login"
 
 ##Configure Payload
 $Body = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $Body.Add("ip","0.0.0.0") 
-$Body.Add("username","billing@vfox.co.uk") 
-$Body.Add("password","zq0N0*vjyyvHI9#!A!v30Jp") 
+$Body.Add("username",$UserName ) 
+$Body.Add("password",$Password) 
 $Body = $Body | ConvertTo-Json
 
 $Bearer = (Invoke-RestMethod -Method Post -Uri $EndPoint -body $Body -ContentType 'application/json' ).data
